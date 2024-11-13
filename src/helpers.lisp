24bbfdd878f2912b61273fdf85c26960c93a26ff
Robert Burnett <dr.slate2@gmail.com>
2024-11-13 10:57:07 -0600

    refined sparse-set implementation

--- src/helpers.lisp
+++ src/helpers.lisp
@@ -7,7 +7,10 @@
    :get-struct-field-names
    :unwrap-fields
    :make-dynamic-array
-   :assert-not-null))
+   :assert-not-null
+   :declaim-type
+   :declare-type
+   :typed-defun))
 
 (in-package :helpers)
 
@@ -48,6 +51,56 @@
 (defun assert-not-null (&rest values)
   (dolist (value values)
     (assert (not (null value)))))
+
+(defmacro declaim-type (name type)
+  `(declaim (type (,type) ,name)))
+
+(defmacro declare-type (name type)
+  `(declare (type (,type) ,name)))
+
+(defmacro declaim-signature (function-name parameter-types return-type)
+  `(declaim (ftype (function ,parameter-types ,return-type) ,function-name)))
+
+(defmacro typed-defun (return-type function-name parameters &body body)
+  "defines a function and declaims its type signature"
+  (assert (evenp (length parameters)))
+  (format t "expanding macro...~%")
+  (let
+      ((parameter-types '())
+       (parameter-names '())
+       (parameter-state :expect-type))
+    (dolist (type-or-name parameters)
+      (case parameter-state
+	(:expect-name
+	 (assert (typep type-or-name 'symbol))
+	 (push type-or-name parameter-names)
+	 (setf parameter-state :expect-type))
+	(:expect-type
+	 (assert (typep type-or-name 'symbol))
+	 (let ((type (find-symbol (symbol-name type-or-name))))
+	   (push type parameter-types))
+	 (setf parameter-state :expect-name))))
+    `(progn
+       (declaim (ftype (function ,(nreverse parameter-types) ,(find-symbol (symbol-name return-type))) ,function-name))
+       (defun ,function-name (,@(nreverse parameter-names)) ,@body))))
+	       
+
+
+(typed-defun :t mynewfun (:string value :integer value2)
+  (format t "~a~%" value)
+  (format t "~a~%" value2)
+  nil)
+
+(mynewfun "hi" (the integer 1))
+
+
+
+
+
+
+
+
+
 
 ;(defun export-all-symbols (package-name)
 ;  (let ((pack (find-package package-name)))
--- src/sparse-set.lisp
+++ src/sparse-set.lisp
@@ -7,6 +7,7 @@
    :ss-set!
    :ss-unset!
    :ss-get
+   :ss-not-nil-sparse
    :make-sparse-set))
 
 (in-package :sparse-set)
@@ -23,6 +24,34 @@
     :initial-element nil
     :adjustable t)))
 
+(typed-defun :vector ss-not-nil-sparse (:sparse-set set)
+  "Returns a list of all non-nil values in the sparse array"
+  (remove-if #'null (ss-sparse set)))
+
+(typed-defun :t ss-audit (:sparse-set set)
+  "audit the sparse set to ensure state is valid"
+  
+  (declare (optimize (safety 3)))
+
+  ;;Assert sparse-set fields are defined
+  (assert-not-null (ss-sparse set) (ss-dense set) (ss-element-type set) (ss-dense-to-sparse set))
+
+  ;Assert there are the same number of dense indexes as non-nil sparse indexes
+  (let
+      ((num-sparse-entries (length (ss-non-nil-sparse set)))
+       (num-dense-entries (fill-pointer (ss-dense set))))
+    (assert (equalp num-sparse-entries num-dense-entries)))
+
+  ;;;Assert all sparse indexes point to a valid and unique dense index
+  (let
+      ((unique-items (list)))
+  (loop for item across (the vector (ss-non-nil-sparse set)) do
+    (assert (not (member item unique-items))) ;ensure uniqueness
+    (assert (>= item 0 )) ;assert gte than 0
+    (assert (< item (fill-pointer (ss-dense set)))) ;assert less than length of dense array
+
+    (push item unique-items))))
+
 (defmacro ss-get-sparse (set sparse-index)
   "Returns a reference to the value in the sparse array"
   `(aref (ss-sparse ,set) ,sparse-index))
@@ -31,24 +60,20 @@
   "Returns a reference to the value in the dense array"
   `(aref (ss-dense ,set) (ss-get-sparse ,set ,sparse-index)))
 
-(defun ss-dense-initialize! (set)
-  "initialize the dense array"
-  (assert-not-null set (ss-element-type set))
+(typed-defun :t ss-dense-initialize! (:sparse-set set)
+  (assert-not-null (ss-element-type set))
   (setf (ss-dense set) (make-dynamic-array (ss-element-type set))))
 
-(defun ss-ensure-dense-initialized! (set)
-  (assert-not-null set)
+(typed-defun :t ss-ensure-dense-initialized! (:sparse-set set)
   "Checks if dense has been initialized and initialize it if necessary"
   (when (null (ss-dense set))
      (ss-dense-initialize! set)))
 
-(defun ss-len (set)
-  (assert-not-null set)
+(typed-defun :integer ss-len (:sparse-set set)
   "Returns the length of the dense array"
   (fill-pointer (ss-dense set)))
 
-(defun ss-top-index (set)
-  (assert-not-null set)
+(typed-defun :integer ss-top-index (:sparse-set set)
   "Returns the index of the top of the dense array"
   (1- (ss-len set)))
 
@@ -60,25 +85,25 @@
   "returns the sparse index pointing to a given dense index"
     `(gethash ,dense-index (ss-dense-to-sparse ,set)))
 
-(defun ss-set-sparse! (set sparse-index value)
+(typed-defun :t ss-set-sparse!
+    (:sparse-set set :integer sparse-index :t value)
   "Sets a value in the sparse array"
-  (assert-not-null set sparse-index)
   (setf (ss-get-sparse set sparse-index) value))
 
-(defun ss-set-dense! (set sparse-index value)
+(typed-defun :t ss-set-dense!
+    (:sparse-set set :integer sparse-index :t value)
   "Sets a value in the dense array"
-  (assert-not-null set sparse-index)
   (setf (ss-get-dense set sparse-index) value))
 
-(defun ss-append-dense! (set value)
+(typed-defun :t ss-append-dense! (:sparse-set set :t value)
   "Appends a new value to the dense array and returns its index"
-  (assert-not-null set value)
+  (assert-not-null value)
 (vector-push-extend value (ss-dense set)))
 
 
-(defun ss-set! (set sparse-index value)
+(typed-defun :t ss-set! (:sparse-set set :integer sparse-index :t value)
   "Add a new value to the set at index"
-  (assert-not-null set sparse-index value
+  (assert-not-null sparse-index value
 		  (ss-element-type set))
   (assert (typep value (ss-element-type set)))
   (ss-ensure-dense-initialized! set)
@@ -91,9 +116,10 @@
 	)
       (ss-set-dense! set sparse-index value)))
 
-(defun ss-unset! (set unset-sparse-index)
-  (let (
-	(unset-dense-index (ss-get-sparse set unset-sparse-index))
+(typed-defun :t ss-unset! (:sparse-set set :integer unset-sparse-index)
+  "Unset the value at index"
+  (let
+      ((unset-dense-index (ss-get-sparse set unset-sparse-index))
 	(top-sparse-index (ss-sparse-from-dense set (ss-top-index set))))
     (setf
      (ss-get-dense set unset-sparse-index)
@@ -113,35 +139,55 @@
 
 (defun ss-test ()
   (let ((set (make-sparse-set :element-type 'integer)))
+
     (format t "Initial set: ~a~%" set)
 
     (format t "Setting 3 to 1...~%")
     (ss-set! set 3 1)
     (format t "set: ~a~%~%" set)
 
+    (ss-audit set)
+
     (format t "Setting 7 to 2...~%")
     (ss-set! set 7 2)
     (format t "set: ~a~%~%" set)
+
+    (ss-audit set)
 
     (format t "Setting 5 to 3...~%")
     (ss-set! set 5 3)
     (format t "set: ~a~%~%" set)
 
+
+    (ss-audit set)
+
+
     (format t "Setting 2 to 5...~%")
     (ss-set! set 2 5)
     (format t "set: ~a~%~%" set)
+
+
+    (ss-audit set)
 
     (format t "Unsetting 3...~%")
     (ss-unset! set 3)
     (format t "~a~%" set)
 
+
+    (ss-audit set)
+
     (format t "Unsetting 2...~%")
     (ss-unset! set 2)
     (format t "~a~%" set)
 
+
+    (ss-audit set)
+
     (format t "Setting 3 to 1...~%")
     (ss-set! set 3 1)
     (format t "set: ~a~%~%" set)
+
+    (ss-audit set)
 
     ))
 
